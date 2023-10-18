5d18cb43b541a71f7658b6fa323b17244798c450
rania-fathallah <87832654+rania-fathallah@users.noreply.github.com>
2023-10-18 23:35:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
+    char buffer[BUFFER_SIZE];
+    FILE *fp;
     char *input = NULL;
     size_t len = 0;
     ssize_t read;
@@ -17,74 +19,71 @@ int main(void)
     pid_t pid;
     int mode = 0;
 
-    while (1)
-    {
-        if (isatty(0))
-            mode = 1;
-        if (mode == 1)
+    while (1) {
+        if (mode == 0) {
+            if (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+                buffer[strcspn(buffer, "\n")] = 0;
+                fp = popen(buffer, "r");
+                if (fp == NULL) {
+                    perror("Error executing command");
+                    exit(EXIT_FAILURE);
+                }
+                while (fgets(buffer, BUFFER_SIZE, fp) != NULL) {
+                    printf("%s", buffer);
+                }
+                pclose(fp);
+            } else {
+                perror("Error reading command");
+                exit(EXIT_FAILURE);
+            }
+        } else {
             write(STDOUT_FILENO, "\033[0;36mhsh# \033[0m", 16);
-        read = getline(&input, &len, stdin);
+            read = getline(&input, &len, stdin);
 
-        if (read < 2)
-        {
-            if (feof(stdin))
-            {
-                printf("\n");
-                break;
-            }
-            else
-            {
-                perror("getline");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        if (input[read - 1] == '\n')
-        {
-            input[read - 1] = '\0';
-        }
-
-        if (strcmp(input, "exit") == 0)
-        {
-            break;
-        }
-
-        token = strtok(input, " ");
-        i = 0;
-        while (token != NULL && i < MAX_ARGS - 1)
-        {
-            args[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-        }
-        args[i] = NULL;
-
-        cmd_path = find_command_path(args[0]);
-
-        if (cmd_path == NULL)
-        {
-            printf("%s: command not found\n", args[0]);
-        }
-        else
-        {
-            pid = fork();
-
-            if (pid == -1)
-            {
-                perror("Fork failed");
-                exit(EXIT_FAILURE);
-            }
-            else if (pid == 0)
-            {
-                if (execve(cmd_path, args, environ) == -1)
-                {
-                    printf("%s: command not found\n", input);
+            if (read < 2) {
+                if (feof(stdin)) {
+                    printf("\n");
+                    break;
+                } else {
+                    perror("getline");
                     exit(EXIT_FAILURE);
                 }
             }
-            else
-            {
-                wait(NULL);
+
+            if (input[read - 1] == '\n') {
+                input[read - 1] = '\0';
+            }
+
+            if (strcmp(input, "exit") == 0) {
+                break;
+            }
+
+            token = strtok(input, " ");
+            i = 0;
+            while (token != NULL && i < MAX_ARGS - 1) {
+                args[i] = token;
+                token = strtok(NULL, " ");
+                i++;
+            }
+            args[i] = NULL;
+
+            cmd_path = find_command_path(args[0]);
+
+            if (cmd_path == NULL) {
+                printf("%s: command not found\n", args[0]);
+            } else {
+                pid = fork();
+                if (pid == -1) {
+                    perror("Fork failed");
+                    exit(EXIT_FAILURE);
+                } else if (pid == 0) {
+                    if (execve(cmd_path, args, NULL) == -1) {
+                        printf("%s: command not found\n", input);
+                        exit(EXIT_FAILURE);
+                    }
+                } else {
+                    wait(NULL);
+                }
             }
         }
     }
@@ -92,4 +91,3 @@ int main(void)
     free(input);
     return 0;
 }
-
