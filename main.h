f20eb718502483415281f0bae6d7aaa73e6e13d5
Mohamed <m.gharieb@gmail.com>
2023-10-20 16:15:22 -0700

    ts v3

--- main.h
+++ main.h
@@ -54,7 +54,7 @@ typedef struct liststr
  *struct passinfo - contains pseudo-arguements to pass into a function,
  *		allowing uniform prototype for function pointer struct
  *@arg: a string generated from getline containing arguements
- *@argv: an array of strings generated from arg
+ *@argv: a vector array of strings generated from arg
  *@path: a string path for the current command
  *@argc: the argument count
  *@line_count: the error count
